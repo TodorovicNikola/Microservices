959287523e190f16f550768c8d18346d69f6bcae
TodorovicNikola <nikola5tod@yahoo.com>
2017-01-19 00:15:16 +0100

    Eureka Service Registry Component Added

---
+++ Eureka_ServiceRegistry/src/main/java/rs/ac/uns/ftn/EurekaServiceRegistryApplication.java
@@ -0,0 +1,14 @@
+package rs.ac.uns.ftn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServiceRegistryApplication.class, args);
+	}
+}
